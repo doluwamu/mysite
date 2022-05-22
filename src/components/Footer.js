12c3f6b8d1364dc70f8a7fed4fd38506ee040f4b
doluwamu <dboyadeitan@gmail.com>
2022-05-22 05:21:03 +0100

    added new features

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,6 +6,7 @@ const Footer = () => {
     <div className="bg-primary py-3" style={{ marginTop: "50px" }}>
       <Container>
         <h3 className="text-white">Doluwamu</h3>
+        <i style={{ color: "#fff" }} className="fas fa-instagram"></i>
       </Container>
     </div>
   );
