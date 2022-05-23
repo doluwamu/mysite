b208353b47bc9ca0a0a847840b2c55febb578e77
doluwamu <dboyadeitan@gmail.com>
2022-05-23 20:40:44 +0100

    implemented contact form

--- src/components/ContactMe.js
+++ src/components/ContactMe.js
@@ -1,7 +1,61 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
 import { Button, Col, Form, Image, Row } from "react-bootstrap";
+import validator from "validator";
 
 const ContactMe = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [subject, setSubject] = useState("");
+  const [message, setMessage] = useState("");
+
+  const [contactRes, setContactRes] = useState("");
+
+  const [nameRequiredError, setNameRequiredError] = useState(false);
+  const [emailRequiredError, setEmailRequiredError] = useState(false);
+  const [emailInvalidError, setEmailInvalidError] = useState(false);
+  const [messageRequiredError, setMessageRequiredError] = useState(false);
+
+  const requiredMsg = "This is a required field!";
+
+  const submitContactInfo = async (e) => {
+    e.preventDefault();
+
+    if (!name || name.length < 1) {
+      return setNameRequiredError(true);
+    }
+    if (!email) {
+      return setEmailRequiredError(true);
+    }
+    if (email && !validator.isEmail(email)) {
+      return setEmailInvalidError(true);
+    }
+    if (!message) {
+      return setMessageRequiredError(true);
+    }
+
+    try {
+      const contactInstance = axios.create({
+        baseURL: "https://doluwamu0r2.herokuapp.com",
+      });
+
+      const { data } = await contactInstance.post("/api/contact", {
+        name,
+        email,
+        subject:
+          subject && subject.length > 0 ? subject : "Just want to connect",
+        message,
+      });
+      setContactRes(data.message);
+      setName("");
+      setEmail("");
+      setSubject("");
+      setMessage("");
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <div className="contact-me">
       <div className="contact-header">
@@ -14,22 +68,53 @@ const ContactMe = () => {
         className="d-flex justify-content-around"
       >
         <Col sm={10} md={5} className="p-3">
-          <Form>
+          <Form onSubmit={submitContactInfo}>
+            {contactRes && contactRes.length > 0 && (
+              <div className="alert alert-success mb-2">
+                <span className="me-auto">{contactRes}</span>
+                <span onClick={() => setContactRes("")}>x</span>
+              </div>
+            )}
+
             <Form.Group className="mb-3" controlId="formBasicPassword">
               <Form.Label className="text-white">Name :</Form.Label>
-              <Form.Control type="text" placeholder="Enter name" />
+              <Form.Control
+                type="text"
+                placeholder="Enter name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
+              {nameRequiredError && (
+                <div style={{ color: "red" }}>!{requiredMsg}*</div>
+              )}
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="formBasicEmail">
               <Form.Label className="text-white">Email :</Form.Label>
-              <Form.Control type="email" placeholder="Enter email" />
+              <Form.Control
+                type="email"
+                placeholder="Enter email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              {emailRequiredError && (
+                <div style={{ color: "red" }}>!{requiredMsg}*</div>
+              )}
+              {emailInvalidError && (
+                <div style={{ color: "red" }}>!Please enter a valid email*</div>
+              )}
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="formBasicPassword">
               <Form.Label className="text-white">
                 Subject (<span style={{ color: "red" }}>optional*</span>) :
               </Form.Label>
-              <Form.Control type="text" placeholder="Enter subject" />
+              <Form.Control
+                type="text"
+                placeholder="Enter subject"
+                value={subject}
+                onChange={(e) => setSubject(e.target.value)}
+              />
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="formBasicPassword">
@@ -39,7 +124,12 @@ const ContactMe = () => {
                 as="textarea"
                 placeholder="Write message"
                 style={{ height: "100px", resize: "none" }}
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
               />
+              {messageRequiredError && (
+                <div style={{ color: "red" }}>!{requiredMsg}*</div>
+              )}
             </Form.Group>
 
             <Button variant="primary" type="submit">
